a8789e4a6d0dffdf2af6f078d68de7a25bca466d
Tedy Gabriel Moisa <moisatedy@gmail.com>
2025-07-06 11:38:38 +0200

    feat: create tables

--- api-gateway/src/config/db.ts
+++ api-gateway/src/config/db.ts
@@ -18,3 +18,33 @@ db.query("SELECT 1")
     logger.error("Database connection failed:", err);
     process.exit(1);
   });
+
+db.query(
+  `
+  CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    short_code VARCHAR(10) UNIQUE NOT NULL,
+    long_url TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    qr_code_path VARCHAR(255)
+  );
+`,
+)
+  .then(() =>
+    db.query(`
+    CREATE TABLE IF NOT EXISTS url_analytics (
+      id SERIAL PRIMARY KEY,
+      url_id INTEGER NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
+      visit_count BIGINT DEFAULT 0,
+      last_visited_at TIMESTAMP WITH TIME ZONE,
+      UNIQUE(url_id)
+    );
+  `),
+  )
+  .then(() => {
+    logger.info("Tables created!");
+  })
+  .catch((err) => {
+    logger.error("Error creating tables:", err);
+    process.exit(1);
+  });
